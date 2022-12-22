14bccb1b7aa10203174a4f813900b4febc7e27c5
dm <dennismok2018@gmail.com>
2022-12-22 22:54:46 +0800

    d6update

--- d6/src/main/kotlin/Main.kt
+++ d6/src/main/kotlin/Main.kt
@@ -5,21 +5,25 @@ import java.util.LinkedList
 
 fun main(){
     val filePath = "/home/dm/projects/aoc_2022/d6/input"
-    val line = File(filePath).bufferedReader().readLine()
     val window = LinkedList<Char>()
     var index = 0
-    line.forEach {
-        window.add(it)
-        println("$index: $window")
-        if (++index >= 14 ){
-            if (window.toSet().size == 14 ){
-                println("char count $index")
-                return
+    File(filePath).inputStream().use { io ->
+        io.readBytes().map {
+            it.toInt().toChar()
+        }.map {
+            window.add(it)
+            println("$index: $window")
+            index++
+            if (window.size == 14 ){
+                if (window.toSet().size == 14 ){
+                    println("char count $index")
+                    return
+                }
+                window.pop()
             }
-            window.pop()
         }
-
     }
+
 }
 
 
